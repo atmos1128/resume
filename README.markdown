9715020a748e5db31d126acc0c83f7daa3efd150
atmos1128 <48702899+atmos1128@users.noreply.github.com>
2019-10-29 21:37:40 +0800

    1

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Resume
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/atmos1128/pen/PooYdqV](https://codepen.io/atmos1128/pen/PooYdqV).
+
+ 
---
+++ script.js
@@ -0,0 +1,33 @@
+$(window).scroll(function(evt){
+  if($(window).scrollTop()>0){
+    $("header").removeClass("at_top");
+  }else{
+    $("header").addClass("at_top");
+  }   
+});
+
+$(window).scroll(function() {
+  if($(window).scrollTop() >= $('#skill').offset().top){
+    $('#bar-1').delay(1000).css('width', '80%');
+    $('#bar-2').delay(1000).css('width', '75%');
+    $('#bar-3').delay(1000).css('width', '70%');
+    $('#bar-4').delay(1000).css('width', '65%');
+    $('#bar-5').delay(1000).css('width', '70%');
+    $('#bar-6').delay(1000).css('width', '65%');
+    $('#bar-7').delay(1000).css('width', '50%');
+  }
+});
+
+$(function(){
+	$('#topBtn').click(function(){ 
+		$('html,body').animate({scrollTop:0}, 333);
+	});
+	$(window).scroll(function() {
+		if ( $(this).scrollTop() > 300 ){
+			$('#topBtn').fadeIn(222);
+		} else {
+			$('#topBtn').stop().fadeOut(222);
+		}
+	}).scroll();
+});
+var s = skrollr.init();
